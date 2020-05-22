d9a82a48748e69838a0e8c748499a36c62d02b23
Sarah Heidmann <sarah.heidmann@gmail.com>
2020-05-22 15:04:19 -0400

    remove boatlog survey controller

--- app/controllers/boatlog_survey_controller.rb
+++
@@ -1,9 +0,0 @@
-class BoatlogSurveyController < ApplicationController
-	before_action :require_manager
-
-	private
-
-	def boatlog_survey_params
-		params.require(:boatlog_survey).permit(:boatlog_id, :user_id, :survey_type_id, :rep)
-	end
-end
