b9fa7add8cac0ed93a528643e68b6b646adbfd1e
Sarah Heidmann <sarah.heidmann@gmail.com>
2020-07-28 12:03:23 -0400

    remove timeentry to fix boatlog validation on focusout

--- app/assets/javascripts/boatlogs.js
+++ app/assets/javascripts/boatlogs.js
@@ -1,13 +1,13 @@
-$(document).on('ready page:load', function() {
+$(document).ready(function() {
   
-	$("#boatlog_begin_time").timeEntry({show24Hours: true, minTime: "08:00", maxTime: "19:00" });
+	// $("#boatlog_begin_time").timeEntry({show24Hours: true, minTime: "06:00", maxTime: "20:00" });
 
-  function alert24HourClock() {
-    $(".boatlog_time").on("focusout", function(){
+	function alert24HourClock() {
+    $("#boatlog_begin_time").on("focusout", function(){
       var $time = $(this).val();
       var b = ($time.split(":")[0]) + ($time.split(":")[1]);
       var $time2 = parseInt(b, 10);
-      if ( $time2 >= "800" && $time2 <= "1900" ){
+      if ( $time2 >= "800" && $time2 <= "1800" ){
       
       } else { alert("Why are you diving in the dark?"); }
     });
@@ -15,13 +15,24 @@ $(document).on('ready page:load', function() {
 
   alert24HourClock();
 
-  /*$("#new_boatlog").validate( {
+	$("#new_boatlog").validate( {
+		debug:true,
   	onfocusout: function(element) {
         this.element(element);
-      },
+    },
 
   	rules: {
-			"boatlog[begin_time]": { required: true }
-  	}
-  });*/
+			"boatlog[site_id]": { required: true },
+			"boatlog[date_completed]": { required: true },
+			"boatlog[begin_time]": { required: true },
+			"boatlog[manager_id]": { required: true }
+  	},
+
+  	messages: {
+      "boatlog[site_id]": { required: "Required" },
+      "boatlog[date_completed]": { required: "Required" },
+      "boatlog[begin_time]": { required: "Required" },
+      "boatlog[manager_id]": { required: "Required" }
+    }
+  });
 });
