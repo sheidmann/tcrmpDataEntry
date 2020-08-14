bc9bdac3b65cbf38805dc849782e671e65ea8205
Sarah Heidmann <sarah.heidmann@gmail.com>
2020-08-14 14:06:52 -0400

    fish model validation

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,4 +1,8 @@
 class Fish < ApplicationRecord
+	validates_presence_of :common_name
+	validates_presence_of :scientific_name
+	validates_presence_of :code_name
+
 	def spp_code_common
     "#{code_name} __ #{common_name}"
   end
--- spec/models/fish_spec.rb
+++ spec/models/fish_spec.rb
@@ -13,5 +13,20 @@ RSpec.describe Fish, type: :model do
   		expect(@name).to eq("COO FISH __ the coolest fish")
   		puts 'new fish has a searchable name'
   	end
+    it "is not valid without a common name" do
+      @fish = build(:fish, common_name: nil)
+      expect(@fish).to_not be_valid
+      puts 'common-nameless fish is not valid'
+    end
+    it "is not valid without a scientific name" do
+      @fish = build(:fish, scientific_name: nil)
+      expect(@fish).to_not be_valid
+      puts 'scientific-nameless fish is not valid'
+    end
+    it "is not valid without a code name" do
+      @fish = build(:fish, code_name: nil)
+      expect(@fish).to_not be_valid
+      puts 'code-nameless fish is not valid'
+    end
   end
 end
