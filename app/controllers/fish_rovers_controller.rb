cd68bc174105287aaac0d27f181cd958987ff4c2
Sarah Heidmann <sarah.heidmann@gmail.com>
2020-09-24 17:06:14 -0400

    view and new fish rovers

--- app/controllers/fish_rovers_controller.rb
+++ app/controllers/fish_rovers_controller.rb
@@ -4,12 +4,29 @@ class FishRoversController < ApplicationController
 	# Create a new fish rover
 	def new
 		@frove = FishRover.new
+    @frove.rover_fishes.build
 
 		respond_to do |format|
   		format.html # new.html.erb
   		format.json { render json: @frove }
   	end
 	end
+
+  def create
+    @frove = FishRover.new( fish_rover_params )
+
+    respond_to do |format|
+      # successful creation
+      if @frove.save
+        format.html { redirect_to @frove, notice: 'Fish rover successfully created.' }
+        format.json { render json: @frove, status: :created, location: @frove }
+      # otherwise, print errors
+      else
+        format.html { render action: "new" }
+        format.json { render json: @frove.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 	# View all of a user's fish rovers
 	def index
@@ -22,10 +39,21 @@ class FishRoversController < ApplicationController
     end
 	end
 
+  # View a fish rover
+  def show
+    @frove = FishRover.find(params[:id])
+    @rovefs = @frove.rover_fishes.all
+
+    respond_to do |format|
+      format.html # show.html.erb
+    end
+  end
+
   private
 
 	def fish_rover_params
 		params.require(:fish_rover).permit(:manager_id, :site_id, :user_id, 
-			:date_completed, :begin_time, :oc_cc, :rep, :notes)
+			:date_completed, :begin_time, :oc_cc, :rep, :notes,
+			rover_fishes_attributes: [:id, :fish_id, :abundance_index, :_destroy])
 	end
 end
--- spec/features/fish_rovers_spec.rb
+++ spec/features/fish_rovers_spec.rb
@@ -19,4 +19,35 @@ describe "User viewing fish rover index", :js => true, type: :feature do
     expect(page).to have_css('tr')
     puts 'user can see fish rovers with option to view'
   end
+  it "can create a fish rover" do
+    visit('/fish_rovers')
+    click_button("New Fish Rover")
+    expect(page).to have_content "Site Name"
+
+    @frove = build(:fish_rover, manager_id: @blmanager.id, site_id: @site.id, user_id: @manager.id)
+    select @blmanager.project, :from => "Project"
+    select @frove.site.site_name, :from => "Site Name"
+    select @frove.user.name, :from => "Observer"
+    fill_in "Date Completed", with: @frove.date_completed.strftime("%Y-%m-%d")
+    fill_in "Begin Time", with: @frove.begin_time.strftime("%H:%M")
+    select @frove.oc_cc, :from => "OC/CC"
+    fill_in "Replicate", with: @frove.rep
+
+    @rovef = build(:rover_fish, fish_id: @fish.id)
+    select @rovef.fish.spp_code_common#, :from => "Species"
+    fill_in("fish_rover_rover_fishes_attributes_0_abundance_index", with: @rovef.abundance_index)
+
+    click_button("Save Fish Rover")
+    expect(page).to have_content "Fish rover successfully created"
+    puts 'user can create a new fish rover'
+  end
+  it "can view a fish rover" do
+    @frove = create(:fish_rover, manager_id: @blmanager.id, site_id: @site.id, user_id: @manager.id)
+    @rovef = create(:rover_fish, fish_rover_id: @frove.id, fish_id: @fish.id)
+    # I can't make Capybara click on the table row, so we just have to visit the link
+    visit("#{fish_rover_path(@frove.id)}")
+    expect(page).to have_content "My Site"
+    expect(page).to have_content "the coolest fish"
+    puts 'user can view fish rover'
+  end
 end
