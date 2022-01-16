2c5bd25451805f819052b4703711cbd40049d47f
Sarah Heidmann <sarah.heidmann@gmail.com>
2022-01-16 10:48:25 -0700

    create barebones controller and views

---
+++ app/controllers/coral_healths_controller.rb
@@ -0,0 +1,24 @@
+class AlgaeHeightsController < ApplicationController
+  before_action :require_user
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
