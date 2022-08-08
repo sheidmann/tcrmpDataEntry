31d60e2d40b19a9291b1faefe3e17fe62740e1dd
Sarah Heidmann <sarah.heidmann@gmail.com>
2022-08-08 11:22:59 -0400

    open select2 dropdown on focus

--- app/assets/javascripts/algae_heights.js
+++ app/assets/javascripts/algae_heights.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
     return;
   };
 
+  // Trigger select2 opening on focus
+  // on first focus (bubbles up to document), open the menu
+  $(document).on('focus', '.select2-selection.select2-selection--single', function (e) {
+    $(this).closest(".select2-container").siblings('select:enabled').select2('open');
+  });
+  // steal focus during close - only capture once and stop propogation
+  $('select.select2').on('select2:closing', function (e) {
+    $(e.target).data("select2").$selection.one('focus focusin', function (e) {
+      e.stopPropagation();
+    });
+  });
+
   // First Section (Metadata)
   // Implement dropdown with textbox for site and observer
   $(".metaSelect").select2();
