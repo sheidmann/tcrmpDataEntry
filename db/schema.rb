e7c498e5e374e8f22467c9dee861831ee903193b
Sarah Heidmann <sarah.heidmann@gmail.com>
2024-05-02 14:27:51 -0400

    add APRO to ESA PA table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_02_165923) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_02_181613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -154,6 +154,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_02_165923) do
     t.string "rami"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "apro"
     t.index ["e_survey_id"], name: "index_e_esa_pas_on_e_survey_id"
   end
 
