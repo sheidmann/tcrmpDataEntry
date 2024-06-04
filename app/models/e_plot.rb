01a8bb0c113ed543106b378eb91ba96e69d60ed9
Sarah Heidmann <sarah.heidmann@gmail.com>
2024-06-04 10:49:24 -0400

    remove habitat from plots in favor of survey

--- app/models/e_plot.rb
+++ app/models/e_plot.rb
@@ -10,13 +10,13 @@ class EPlot < ApplicationRecord
   accepts_nested_attributes_for :e_plot_corals
 
   def self.as_csv
-    columns = %w(fid date_completed diver team role habitat hardbottom coral_cover max_vert_relief_cm min_depth max_depth)
+    columns = %w(fid proofed date_completed diver buddy team role habitat hardbottom coral_cover max_vert_relief_cm min_depth max_depth)
     CSV.generate(headers: true) do |csv|
       csv << columns.map(&:humanize)
       all.each do |p|
         esurv = p.e_survey
         diver = esurv.user
-        csv << [esurv.fid, esurv.date_completed, diver.name, esurv.team, esurv.role, p.habitat, p.hardbottom, p.coral_cover, p.max_relief_cm, p.min_depth, p.max_depth]
+        csv << [esurv.fid, esurv.proofed, esurv.date_completed, diver.name, esurv.buddy, esurv.team, esurv.role, esurv.habitat, p.hardbottom, p.coral_cover, p.max_relief_cm, p.min_depth, p.max_depth]
       end
     end
   end
