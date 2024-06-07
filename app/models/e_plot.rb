bf5eddcca9a4138cdd7824253a89a59a3d240de5
Sarah Heidmann <sarah.heidmann@gmail.com>
2024-06-07 09:40:33 -0400

    add ECMP plot num to abiotic index and export

--- app/models/e_plot.rb
+++ app/models/e_plot.rb
@@ -10,13 +10,13 @@ class EPlot < ApplicationRecord
   accepts_nested_attributes_for :e_plot_corals, :reject_if => :all_blank, :allow_destroy => true
 
   def self.as_csv
-    columns = %w(fid proofed date_completed diver buddy team role habitat hardbottom coral_cover max_vert_relief_cm min_depth max_depth)
+    columns = %w(fid proofed date_completed diver buddy team role habitat plot hardbottom coral_cover max_vert_relief_cm min_depth max_depth)
     CSV.generate(headers: true) do |csv|
       csv << columns.map(&:humanize)
       all.each do |p|
         esurv = p.e_survey
         diver = esurv.user
-        csv << [esurv.fid, esurv.proofed, esurv.date_completed, diver.name, esurv.buddy, esurv.team, esurv.role, esurv.habitat, p.hardbottom, p.coral_cover, p.max_relief_cm, p.min_depth, p.max_depth]
+        csv << [esurv.fid, esurv.proofed, esurv.date_completed, diver.name, esurv.buddy, esurv.team, esurv.role, esurv.habitat, p.plot, p.hardbottom, p.coral_cover, p.max_relief_cm, p.min_depth, p.max_depth]
       end
     end
   end
