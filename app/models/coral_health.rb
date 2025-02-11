24380884c1eae1dc28b0e17ae101bcd99f7989ee
Sarah Heidmann <sarah.heidmann@gmail.com>
2025-02-11 16:27:32 -0400

    mods for ease of post-processing

--- app/models/coral_health.rb
+++ app/models/coral_health.rb
@@ -25,7 +25,7 @@ class CoralHealth < ApplicationRecord
   end
 
   def self.as_csv
-    columns = %w(coral_health_id proofed site_name date_completed method name rep site_notes transect_coral_id coral_code_name coral_full_name length_cm width_cm height_cm bl_sp bl_p bl_vp bl_bl old_mortality recent_mortality coral_notes interaction_name interaction_value)
+    columns = %w(coral_health_id proofed site_name date_completed method name rep site_notes transect_coral_id coral_code_name coral_long_code coral_full_name length_cm width_cm height_cm bl_sp bl_p bl_vp bl_bl old_mortality recent_mortality colony_notes interaction_name interaction_group interaction_category interaction_value)
     CSV.generate(headers: true) do |csv|
       csv << columns.map(&:humanize)
       all.each do |coral_health|
@@ -34,7 +34,7 @@ class CoralHealth < ApplicationRecord
         coral_health.transect_corals.each do |transect_coral|
           coral = transect_coral.coral_code
           # Create the row through coral dimensions
-          row_coral = [coral_health.id, coral_health.proofed, site.site_name, coral_health.date_completed, coral_health.method, user.name, coral_health.rep, coral_health.notes, transect_coral.id, coral.code_name, coral.full_name, transect_coral.length_cm, transect_coral.width_cm, transect_coral.height_cm, transect_coral.bl_sp, transect_coral.bl_p, transect_coral.bl_vp, transect_coral.bl_bl, transect_coral.old_mortality, transect_coral.new_mortality, transect_coral.notes]
+          row_coral = [coral_health.id, coral_health.proofed, site.site_name, coral_health.date_completed, coral_health.method, user.name, coral_health.rep, coral_health.notes, transect_coral.id, coral.code_name, coral.long_code, coral.full_name, transect_coral.length_cm, transect_coral.width_cm, transect_coral.height_cm, transect_coral.bl_sp, transect_coral.bl_p, transect_coral.bl_vp, transect_coral.bl_bl, transect_coral.old_mortality, transect_coral.new_mortality, transect_coral.notes]
           # Leave interaction columns empty if none
           if transect_coral.coral_interactions.count == 0
             csv << row_coral
@@ -42,7 +42,7 @@ class CoralHealth < ApplicationRecord
             # Otherwise add a full row for each interaction
             transect_coral.coral_interactions.each do |coral_interaction|
               interaction = coral_interaction.coral_code
-              csv << row_coral + [interaction.code_name] + [coral_interaction.value]
+              csv << row_coral + [interaction.code_name] + [interaction.group] + [interaction.category] + [coral_interaction.value]
             end
           end
         end
